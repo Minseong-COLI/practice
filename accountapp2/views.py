3d470dcf211dc1e1bb97e79734876df18cf58d11
als90 <als90915@gmail.com>
2021-07-20 17:04:26 +0900

    2021.07.20 commit_1

--- accountapp2/views.py
+++ accountapp2/views.py
@@ -12,29 +12,32 @@ from accountapp2.forms import AccountCreationForm
 
 
 def hello_coli(request):
-    # return HttpResponse('Hell Coli')
-    # return render(request, 'base.html')
-    if request.method == "POST":
+    if request.user.is_authenticated:
+        # return HttpResponse('Hell Coli')
+        # return render(request, 'base.html')
+        if request.method == "POST":
 
-        temp = request.POST.get('input_text')
+            temp = request.POST.get('input_text')
 
-        new_hello_coli = HelloColi()
-        new_hello_coli.text = temp
-        new_hello_coli.save()
-        # 객체를 DB에 저장    # temp와 다르게 DB에 저장됨(왼쪽 파일 목록 db.sqlite3)
-        # settings.py의 DATABASE 내용 확인 해보면 지정된걸 확인할수있음
+            new_hello_coli = HelloColi()
+            new_hello_coli.text = temp
+            new_hello_coli.save()
+            # 객체를 DB에 저장    # temp와 다르게 DB에 저장됨(왼쪽 파일 목록 db.sqlite3)
+            # settings.py의 DATABASE 내용 확인 해보면 지정된걸 확인할수있음
 
-        return HttpResponseRedirect(reverse('accountapp2:hello_coli'))
+            return HttpResponseRedirect(reverse('accountapp2:hello_coli'))
 
-        # hello_world_list = HelloWorld.objects.all()
-        # # DB의 모든 데이터를 hello_world_list에 넣어줌
-        #
-        # return render(request, 'accountapp/hello_world.html',
-        #               context={'hello_world_list': hello_world_list})
+            # hello_world_list = HelloWorld.objects.all()
+            # # DB의 모든 데이터를 hello_world_list에 넣어줌
+            #
+            # return render(request, 'accountapp/hello_world.html',
+            #               context={'hello_world_list': hello_world_list})
+        else:
+            hello_coli_list = HelloColi.objects.all()
+            return render(request, 'accountapp2/hello_coli.html',
+                          context={'hello_coli_list': hello_coli_list})
     else:
-        hello_coli_list = HelloColi.objects.all()
-        return render(request, 'accountapp2/hello_coli.html',
-                      context={'hello_coli_list': hello_coli_list})
+        return HttpResponseRedirect(reverse('accountapp2:login'))
 
 
 class AccountCreateView(CreateView):
@@ -43,6 +46,18 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp2:hello_coli')
     template_name = 'accountapp2/create.html'
 
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp2:login'))
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp2:login'))
+
 
 class AccountDetailView(DetailView):
     model = User
@@ -50,6 +65,18 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     # user를 타겟으로 가져온다
     template_name = 'accountapp2/detail.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp2:login'))
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('accountapp2:login'))
 
 
 class AccountUpdateView(UpdateView):
