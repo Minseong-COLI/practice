5086d673ffd980da60502fad586abcf3406a5696
als90 <als90915@gmail.com>
2021-08-23 11:48:12 +0900

    2021.08.23 commit_2

--- projectapp/views.py
+++ projectapp/views.py
@@ -9,6 +9,7 @@ from django.views.generic import CreateView, DetailView, ListView
 from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
+from subscribeapp.models import Subscription
 
 
 @method_decorator(login_required, 'get')
@@ -28,8 +29,18 @@ class ProjectDetailView(DetailView):
     template_name = 'projectapp/detail.html'
 
     def get_context_data(self, **kwargs):
+        user = self.request.user
+        project = self.object
+
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user, project=project)
+        else:
+            subscription = None
+
         article_list = Article.objects.filter(project=self.object)
-        return super().get_context_data(object_list=article_list, **kwargs)
+        return super().get_context_data(object_list=article_list,
+                                        subscription=subscription,
+                                        **kwargs)
 
 
 class ProjectListView(ListView):
