6cad69b954b5d4a9d060840dee672017e0da6e61
als90 <als90915@gmail.com>
2021-09-27 13:06:01 +0900

    2021.09.27 commit_4

--- practice/settings/base.py
+++ practice/settings/base.py
@@ -18,24 +18,8 @@ from django.urls import reverse_lazy
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-env_list = dict()
-local_env = open(os.path.join(BASE_DIR, '.env'))
-# os: 운영체제, path: 경로, join: 합치기 메서드, base_dir: settings.py의 부모+부모 경로->venv 폴더, .env 파일을 경로지정
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')  # 줄바꿈->공백으로 치환
-    start = line.find('=')  # =의 위치를 파악해서 그 index를 start라는 변수 에 저장
-    key = line[:start]  # start('=')를 기준으로 슬라이싱-> key, value 지정
-    value = line[start:]
-    env_list[key] = value  # env_list 딕셔너리에 key, value 값 저장
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xwh=9@-9%4!awin6w_7a%s7sn9wl8i#^8q$ns#*bahtx25k*9r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
